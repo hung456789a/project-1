7d00360501a98d565c213b16363f139e4929647e
hung456789a <hung456789a@gmail.com>
2025-05-09 09:06:29 +0700

    update

--- wait-for-mongo.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-# Đợi đến khi MongoDB lắng nghe trên cổng 27017
-echo "Waiting for MongoDB to be ready..."
-until nc -z mongo 27017; do
-  sleep 1
-done
-echo "MongoDB is ready. Starting the app..."
-exec java -jar app.jar
